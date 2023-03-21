980cf703afe4625cfa5769c639977cf28e314cee
Tristan <tristanskyhook@gmail.com>
2023-03-21 11:50:42 -0700

    1

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -4,6 +4,28 @@ class Vessel
   def initialize(name, volume)
     @name = name
     @volume = volume
+    @fullness = 0
+  end
+
+  def name
+    @name
+  end
+
+  def volume
+    @volume
+  end
+
+  def fill(volume = @volume)
+    if volume >= @volume
+      @fullness = @volume
+    else
+      @fullness += volume
+    end
+
+  end
+
+  def empty?
+    @fullness == 0
   end
 
 end
