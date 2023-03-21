85dadb24074c2511af81eb2e36304d99ecf71c07
Tristan <tristanskyhook@gmail.com>
2023-03-21 13:54:12 -0700

    item

--- lib/item.rb
+++ lib/item.rb
@@ -1,9 +1,52 @@
 class Item
   attr_reader :name, :volume
 
-  def initialize(name, volume)
+  def initialize(name = "Mystery Treat", volume = 1, age = 0, spoil = false)
     @name = name
     @volume = volume
+    @age = age
+    @spoil = spoil
+  end
+
+  def eat(volume = @volume)
+    if self.edible? == false
+      raise "You have food poisoning!"
+    end
+
+    if volume >= @volume
+      #delete?
+      @volume = 0
+    else
+      @volume -= volume
+    end
+  end
+
+  def older
+    @age += 1
+  end
+
+  def edible?
+    if @spoil == false or @spoil >= @age
+      return true
+    else
+      return false
+    end
+  end
+
+  def age
+    @age
+  end
+
+  def name
+    @name
+  end
+
+  def volume
+    @volume
+  end
+
+  def spoil
+    @spoil
   end
 
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,4 +2,45 @@ require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
 
+    it 'has a name and volume' do
+        item = Item.new('FAKE', 100)
+        expect(item.name).to eq('FAKE')
+        expect(item.volume).to eq(100)
+    end
+
+    it 'has a default name and volume' do
+        item = Item.new
+        expect(item.name).to eq('Mystery Treat')
+        expect(item.volume).to eq(1)
+        expect(item.age).to eq(0)
+        expect(item.spoil).to eq(false)
+    end
+
+    it 'can be eaten' do
+        item = Item.new('FAKE', 100)
+        item.eat
+        expect(item.volume).to eq(0)
+    end
+
+    it 'can be eaten in smaller amounts' do
+        item = Item.new('FAKE', 100)
+        item.eat(50)
+        expect(item.volume).to eq(50)
+    end
+
+    it 'can get older' do
+        item = Item.new('FAKE', 100)
+        item.older
+        expect(item.age).to eq(1)
+    end
+
+    it 'can be edible' do
+        item = Item.new('FAKE', 100)
+        expect(item.edible?).to eq(true)
+    end
+
+    it 'can kill you' do
+        item = Item.new('FAKE', 100, 100, 100)
+        expect(item.edible?).to eq(true)
+    end
 end
